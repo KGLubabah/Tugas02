036ba575882c282a0f1880284ad7be8b64fb30e6
USER PC <USER PC@DESKTOP-6ESNFKL>
2018-09-09 20:24:11 +0700

    Initial Commit

---
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -0,0 +1,15 @@
+package DynamicProgramming;
+import java.util.Scanner;
+public class HitungFibonacci {
+    public static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+        String identitas = "Khairis Ghifariy Lubabah / XR3 / 15";
+        tampilJudul(identitas);
+    
+}
