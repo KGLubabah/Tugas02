8d6779eecde7129be92f1b31ea10d8e9292398ef
USER PC <USER PC@DESKTOP-6ESNFKL>
2018-09-14 20:39:35 +0700

    Tambah Hitung Jumlah Uang

--- src/DynamicProgramming/Tugas01.java
+++ src/DynamicProgramming/Tugas01.java
@@ -12,4 +12,26 @@ public class Tugas01 {
         
         return uang;
     }
+    
+    private static void JumlahUang(int uang, int[] data)
+    {
+        for (int i = 0; i < data.length; i++) 
+        {
+            int N = 0;
+            while (uang >= data[i])
+            {
+                uang = uang - data[i];
+                N++;
+            }
+            
+            if (data[i] == 5000 || data[i] == 2000 || data[i] == 1000)
+            {    
+                System.out.println("Banyak Pecahan " + data[i] + " Sebanyak : " + N + " Lembar");
+            }
+            else
+            {
+                System.out.println("Banyak Pecahan " + data[i] + " Sebanyak : " + N + " Koin");
+            }
+        }
+    }
 }
