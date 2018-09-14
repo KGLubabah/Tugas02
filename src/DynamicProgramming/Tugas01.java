deed3c01f8251176842ca17f4c46bbd1f75c6843
USER PC <USER PC@DESKTOP-6ESNFKL>
2018-09-14 20:38:28 +0700

    Tambah Input

---
+++ src/DynamicProgramming/Tugas01.java
@@ -0,0 +1,15 @@
+package DynamicProgramming;
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Tugas01 {
+    private static int input()
+    {
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Masukkan Jumlah Uang  Anda : ");
+        int uang = baca.nextInt();
+        System.out.println("");
+        
+        return uang;
+    }
+}
