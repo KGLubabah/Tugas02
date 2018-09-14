a89f0f507ef8f3a3e73554edd252c49d15753aa7
USER PC <USER PC@DESKTOP-6ESNFKL>
2018-09-14 20:47:16 +0700

    Tambah Proses Hitung

--- src/DynamicProgramming/Tugas02.java
+++ src/DynamicProgramming/Tugas02.java
@@ -25,5 +25,35 @@ public class Tugas02 {
         System.out.print("Berat Max : ");
         n = baca.nextInt(); 
         System.out.println("");
+        
+    
+        while (n >= 7)
+        {
+            n = n - 7;
+            e++;
+        }
+        System.out.println("Banyak Uang Berjenis Barang E Sebanyak : Rp" + (e * 10000));
+        
+        while (n >= 5)
+        {
+            n = n - 5;
+            b++;
+        }
+        System.out.println("Banyak Uang Berjenis Barang B Sebanyak : Rp" + (b * 7000));
+        
+        while (n >= 3)
+        {
+            n = n - 3;
+            d++;
+        }
+        System.out.println("Banyak Uang Berjenis Barang D Sebanyak : Rp" + (d * 3000));
+        
+        while (n >= 2)
+        {
+            n = n - 2;
+            c++;
+        }
+        System.out.println("Banyak Uang Berjenis Barang C Sebanyak : Rp" + (c * 2000));
+        System.out.println("");
     }
 }
